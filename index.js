093a9031ebf4c9a7ec95ede230066b70e0697c2c
Arash <Arshiamidos@github.com>
2018-02-13 01:18:52 +0330

    .

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('peeyade', () => App);
+AppRegistry.registerComponent('PeeyadeCHI', () => App);
