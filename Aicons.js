4ac4358f9ef282ee7ad0315bbdf0e0d7b0805981
Arash <Arshiamidos@gmail.com>
2018-02-17 22:29:28 +0330

    new update for repository place
    :) so tricky way
    just moving .git folder

--- Aicons.js
+++ Aicons.js
@@ -7,6 +7,11 @@ export default class IconExample extends Component {
         <Header />
         <Content>
           <Icon name='home' />
+          <Icon name='keypad' />
+          <Icon name='alarm' />
+          <Icon name='medkit' />
+          <Icon name='refresh' />
+          <Icon name='cog' />
           <Icon ios='ios-menu' android="md-menu" style={{fontSize: 20, color: 'red'}}/>
         </Content>
       </Container>
