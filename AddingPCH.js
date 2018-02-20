cba4f78ccbaf7778417712eae8eebb1ae5da3b91
Arash <Arshiamidos@gmail.com>
2018-02-20 22:19:03 +0330

    starting with WebView
    starting webview with HTML5 for post and use in react native !!

--- AddingPCH.js
+++ AddingPCH.js
@@ -1,15 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import { ButtonGroup } from 'react-native-elements'
-import { Dropdown } from 'react-native-material-dropdown';
-import { Button,SearchBar } from 'react-native-elements';
-import { Container, Header, Item, Input } from 'native-base';
-import SelectMultiple from 'react-native-select-multiple'
 import {
   Platform,
   StyleSheet,
@@ -17,9 +6,16 @@ import {
   View,
   Icon,
   TextInput,
-  ListView
+  ListView,
+  Alert,
+  Dimmensions,
 //  Button
 } from 'react-native';
+import { ButtonGroup } from 'react-native-elements'
+import { Dropdown } from 'react-native-material-dropdown';
+import { Button,SearchBar } from 'react-native-elements';
+import { Container, Header, Item, Input } from 'native-base';
+import SelectMultiple from 'react-native-select-multiple'
 
 
 const instructions = Platform.select({
@@ -39,6 +35,11 @@ export default class App extends Component<Props> {
       fruits : ['honda','kotlin','آرش توکلی', 'ساتوشی', 'ناکامورا','استیو وزنیاک', 'جابز امپرالیست', 'بیل گیتس'],
       selectedFruits: [],
       selectedIndex: 2,
+      name:'',
+      desiredRole: 'author',
+      mainRole: 'admin',
+      mainCatogry:'',
+      skills:'',
       text:'جستجو',
       pic:"https://facebook.github.io/react-native/docs/assets/favicon.png",
       dataSource: ds.cloneWithRows([
@@ -52,6 +53,8 @@ export default class App extends Component<Props> {
     };
     this.addOne = this.addOne.bind(this)
     this.setdata=this.setdata.bind(this)
+    this.onSelectedSearchName=this.onSelectedSearchName.bind(this)
+    this.onSelectedSearchSkills=this.onSelectedSearchSkills.bind(this)
   }
   setdata=(res)=>{
    // this.setState({bons:res.data.user.bons})
@@ -73,6 +76,20 @@ export default class App extends Component<Props> {
   }
   addOne=()=> {
     try {
+      if(this.state.name=='' && this.state.skills=='' )
+      {
+        Alert.alert(
+          'خطا',
+          'لطفا حداقل یکی از موارد جستجو را پر کنید',
+          [
+            /* {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')}, */
+/*             {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+ */            {text: 'باشه', onPress: () => console.log('OK Pressed')},
+          ],
+          { cancelable: false }
+        );
+        return;
+      }
       let ress='xxx'
       fetch(
         'https://peeyade.com/api/pch/v1/users/search?limit=10,offset=0',{  
@@ -84,10 +101,10 @@ export default class App extends Component<Props> {
           },
           body: JSON.stringify({
             activityTags: '',
-            mainActivity: '',
-            name: 'آرش',
-            desiredRole: 'author',
-            mainRole: 'admin',
+            mainActivity: this.state.mainActivity,
+            name: this.state.name,
+            desiredRole: this.state.desiredRole,
+            mainRole: this.state.mainRole,
 
           })
         
@@ -114,10 +131,13 @@ componentDidMount(){
 }
 
   onSelectionsChange = (selectedFruits,item) => {
-    // selectedFruits is array of { label, value }
     this.setState({ selectedFruits })
-    //item.style={backgroundColor:'gray'}
   }
+
+  onSelectedSearchName = (selectedSearchName) => {this.setState({ name:selectedSearchName })}
+  //onSelectedSearchMainRole = (changedText) => {this.setState({ mainRole:changedText })}
+  //onSelectedSearchMainCatogry = (changedText) => {this.setState({ mainActivity:changedText })}
+  onSelectedSearchSkills = (changedText) => {this.setState({ mainActivity:changedText })}
 
   updateIndex (selectedIndex) {
     this.setState({selectedIndex})
@@ -138,7 +158,7 @@ componentDidMount(){
           icon={{ type: 'font-awesome', name: 'search' }}
           round
             lightTheme={true}
-            onChangeText={()=>{}}
+            onChangeText={this.onSelectedSearchName}
             onClearText={()=>{}}
             inputStyle={{textAlign:'center'}}
             containerStyle={{direction:'rtl'}}
@@ -184,7 +204,7 @@ componentDidMount(){
           containerStyle={{direction:'rtl',flex:10}}
           placeholder=' کتگوری اصلی ' />
           <Text style={{flex:2,alignSelf:'flex-end',padding:10}}>
-          کتگوری اصلی 
+          کتگوری  
           </Text>
         </View>
 
@@ -195,7 +215,7 @@ componentDidMount(){
           //icon={{ type: 'font-awesome', name: 'search' }}
           round
             lightTheme={true}
-            onChangeText={()=>{}}
+            onChangeText={this.onSelectedSearchSkills}
             onClearText={()=>{}}
             inputStyle={{textAlign:'center'}}
             containerStyle={{direction:'rtl',flex:10}}
@@ -207,21 +227,33 @@ componentDidMount(){
        
         <Button
              buttonStyle={styles.buttonStyle}
-             title="تایید"
+             title="جستجو"
              onPress={()=>this.addOne()}
         />
        
-        
-      <Text style={{alignSelf:'flex-end',padding:10}}>
-       پیاده‌چی ها 
-      </Text>
-
+      <View style={{flexDirection:'row'}}>  
+        <Text style={{flex:2,alignSelf:'flex-start',}}>
+        پیاده‌چی ها 
+        </Text>
+        <Text style={{flex:1,alignSelf:'center',}}>
+        پیاده‌چی ها 
+        </Text>
+      </View>  
+      <View style={{}}>
       <SelectMultiple
-          style={{direction:'rtl'}}
+          style={{direction:'rtl',height: 300}}
           items={this.state.fruits}
           selectedItems={this.state.selectedFruits}
+          selectedLabelStyle={{color:'white'}}
           selectedRowStyle={{backgroundColor:'gray'}}
           onSelectionsChange={this.onSelectionsChange} />
+        </View>
+       <Button
+            style={{flex:1}}
+             buttonStyle={styles.buttonStyle}
+             title="تایید"
+             onPress={()=>{}}
+        />
       </View>
     )
   }
